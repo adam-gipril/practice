a4efc4f336c1f917f23a27c0a2231c3244bb3346
Adam Gipril <adam.gipril@ibotta.com>
2024-01-03 12:18:46 -0700

    Remove NPM step from release

--- release.config.js
+++ release.config.js
@@ -14,19 +14,6 @@ module.exports = {
        */
       '@semantic-release/release-notes-generator',
       /**
-       * Publishes to NPM and updates the version number in package*.json files.
-       * The actual publish step gets skipped due to `"private": true` in package.json.
-       *
-       * @see https://github.com/semantic-release/npm
-       */
-      '@semantic-release/npm',
-      /**
-       * Commits version number increments back to `main`.
-       *
-       * @see https://github.com/semantic-release/git#readme
-       */
-      ['@semantic-release/git', { assets: ['package.json', 'package-lock.json'] }],
-      /**
        * Publishes a GitHub release.
        *
        * @see https://github.com/semantic-release/github#readme
